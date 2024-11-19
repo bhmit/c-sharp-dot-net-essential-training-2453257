f35901e30c749e0a3887c0000d618e3fa28f9625
Bandar Madani <b.madane@gmail.com>
2024-11-19 13:12:39 +0300

    mYBandar

---
+++ Start/Overview/MyConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(".net v = 8");
+
+
+string response;
+Console.WriteLine("What is your name?");
+response = Console.ReadLine();
+Console.WriteLine($"Your name is:{response}. Engoy the course!");
+
+OperatingSystem thisOs = Environment.OSVersion;
+Console.WriteLine(thisOs);
+Console.WriteLine(thisOs.Platform);
+Console.WriteLine(thisOs.VersionString);
